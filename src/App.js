3e3ab2af6cc9df8d111c73169d8a414e59b921a8
Elgiz Baloghlanli <elgiz394@gmail.com>
2023-05-01 20:01:42 +0400

    added custom hooks

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts'
+import useWindowSize from "./hooks/useWindowSize";
+import useAxiosFetch from "./hooks/useAxiosFetch";
 
 
 function App() {
@@ -20,26 +22,13 @@ function App() {
   const [editTitle, setEditTitle] = useState('')
   const [editBody, setEditBody] = useState('')
   const navigate = useNavigate();
+  const {width} = useWindowSize();
+
+  const {data, fetchError, isLoading} = useAxiosFetch('http://localhost:3500/posts')
 
   useEffect(() => {
-    const fetchPosts = async() => {
-      try{
-        const response = await api.get('/posts')
-        setPosts(response.data)
-      } catch (err){
-        //Not in the 200 response range
-        if(err.response){
-          console.log(err.response.data)
-          console.log(err.response.status)
-          console.log(err.response.headers)
-        } else {
-          console.log(`Error : ${err.message}`)
-        }
-      }
-    }
-
-    fetchPosts()
-  },[])
+    setPosts(data);
+  },[data])
 
   useEffect(() => {
     const filteredResults = posts.filter(post =>
@@ -94,8 +83,15 @@ function App() {
   return (
 
     <Routes>
-      <Route path="/" element={<Layout search={search} setSearch={setSearch} />}>
-        <Route index element={<Home posts={searchResults} />} />
+      <Route path="/" element={<Layout search={search} setSearch={setSearch} width={width}/>}>
+        <Route 
+          index 
+          element={<Home 
+                      posts={searchResults} 
+                      fetchError={fetchError}
+                      isLoading={isLoading}
+                  />} 
+        />
 
         <Route path="post">
           <Route index element={<NewPost
